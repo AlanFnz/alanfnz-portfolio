751b0ec8f726f6507c0d813ee3e53ea4662f2c43
Alan Fernandez <alan.f@msn.com>
2021-01-27 12:29:30 -0300

    Created backend. Added project links

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 // Video
-import beatmonMp4 from '../img/beatmon-video.mp4';
-import gearwavesMp4 from '../img/gearwaves-video.mp4';
-import ixtlanMp4 from '../img/ixtlan-clothing-video.mp4';
+import beatmonMp4 from "../img/beatmon-video.mp4";
+import gearwavesMp4 from "../img/gearwaves-video.mp4";
+import ixtlanMp4 from "../img/ixtlan-clothing-video.mp4";
 
 const Popup = (props) => {
   const { project, isOpen, toggle } = props;
-  const videoSrc = project.name === 'Beatmon' ? beatmonMp4 : project.name === 'Gearwaves' ? gearwavesMp4 : ixtlanMp4
+  const videoSrc =
+    project.name === "Beatmon"
+      ? beatmonMp4
+      : project.name === "Gearwaves"
+      ? gearwavesMp4
+      : ixtlanMp4;
 
-  const EmbedVideo = function(props) {
+  const EmbedVideo = function (props) {
     return (
-        <div dangerouslySetInnerHTML={{ __html: `
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
          <video
            loop
            muted
@@ -19,26 +26,61 @@ const Popup = (props) => {
            src="${props.src}"
            class="${props.className}"
          />,
-       ` }}></div>
-    )
- }
-  
+       `,
+        }}
+      ></div>
+    );
+  };
+
+  const sourceButtonsMarkup = project.sourceClient ? (
+    <Fragment>
+      <a
+        href={project.sourceClient}
+        className="btn btn--primary u-margin-left-small btn--popup"
+        target="_blank" rel="noreferrer"
+      >
+        Client Code
+      </a>
+      <a
+        href={project.sourceAPI}
+        className="btn btn--primary u-margin-left-small btn--popup"
+        target="_blank" rel="noreferrer"
+      >
+        API Code
+      </a>
+    </Fragment>
+  ) : (
+    <a
+      href={project.source}
+      className="btn btn--primary u-margin-left-small btn--popup"
+      target="_blank" rel="noreferrer"
+    >
+      Source code
+    </a>
+  );
+
   return (
-    <div className={`popup ${isOpen ? 'popup--open' : ''}`} id="popup">
+    <div className={`popup ${isOpen ? "popup--open" : ""}`} id="popup">
       <div className="popup__content">
         <div className="popup__top">
           <EmbedVideo src={videoSrc} className="popup__video" />
         </div>
         <div className="popup__bottom">
           <div className="popup__close-background">&nbsp;</div>
-          <div className="popup__close" onClick={toggle}>&times;</div>
-          <h2 className="heading-secondary u-margin-bottom-small">{project.name}</h2>
-          <h3 className="heading-tertiary u-margin-bottom-small">{project.summary}</h3>
-          <p className="popup__text">
-            {project.description}
-          </p>
-          <a href="#" className="btn btn--primary btn--popup">Live Demo</a>
-          <a href="#" className="btn btn--primary u-margin-left-small btn--popup">Source code</a>
+          <div className="popup__close" onClick={toggle}>
+            &times;
+          </div>
+          <h2 className="heading-secondary u-margin-bottom-small">
+            {project.name}
+          </h2>
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            {project.summary}
+          </h3>
+          <p className="popup__text">{project.description}</p>
+          <a href={project.live} className="btn btn--primary btn--popup" target="_blank" rel="noreferrer">
+            Live Demo
+          </a>
+          {sourceButtonsMarkup}
         </div>
       </div>
     </div>
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const compression = require('compression');
+const enforce = require('express-sslify');
+const sgMail = require('@sendgrid/mail')
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+sgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true}));
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
+});
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+
+app.post('/message', (req, res, next) => {
+  const from = req.body.email;
+  const message = `Message from the portfolio by ${from}: ${req.body.message}`;
+
+  var msg = {
+    to: 'alan.ixtlan@gmail.com',  
+    from: 'alan.f@msn.com',
+    subject: 'Contact from Ixtlan Clothing',
+    text: message
+  }
+
+  sgMail
+  .send(msg)
+  .then(() => {
+    res.status(200).json({ status: 'success' });
+  })
+  .catch((error) => {
+    console.error(error)
+    res.status(500).json({ status: 'error' });
+  })
+
+})
